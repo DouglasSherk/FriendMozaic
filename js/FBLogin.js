8cb88d4348c1e1a0421c9868b3749696d45a3ec3
PPYMOU <ppymou@gmail.com>
2012-01-21 00:12:14 -0500

    Basic Login

--- js/FBLogin.js
+++ js/FBLogin.js
@@ -1,4 +1,26 @@
-    FB.init({appId: '158930817550508'});
+<div id="fb-root"></div>
+        <script>
+            window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '158930817550508', // App ID
+                channelUrl : '//WWW.YOUR_DOMAIN.COM/channel.html', // Channel File
+                status     : true, // check login status
+                cookie     : true, // enable cookies to allow the server to access the session
+                xfbml      : true  // parse XFBML
+            });
+
+        // Additional initialization code here
+        };
+
+        // Load the SDK Asynchronously
+        (function(d) {
+            var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+            js = d.createElement('script'); js.id = id; js.async = true;
+            js.src = "//connect.facebook.net/en_US/all.js";
+            d.getElementsByTagName('head')[0].appendChild(js);
+            }(document));
+        </script>
+            FB.init({appId: '158930817550508'});
                 
     function login() {
         FB.login(function(response) {
@@ -36,8 +58,7 @@
           $("#show-friends").show();
 
       });
-    }              
-
+    }
     
     var friendURL = new Array();   
 
@@ -63,3 +84,6 @@
 
         window.console && console.log("in showfriends4!"); 
     });
+        </script>
+        
+        

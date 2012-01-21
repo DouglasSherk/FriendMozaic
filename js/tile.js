1f703fcd67fe09eac50eaf915fa7aa42740e1554
Doug Sherk <github@sherk.me>
2012-01-21 17:45:28 +0000

    add saving

--- js/tile.js
+++ js/tile.js
@@ -71,7 +71,7 @@
                 width = profilePic.width - (profilePic.width % tileX);
             }
 
-            $(".contentslider").height(height + 50);
+            $(".contentslider").height(height + 150);
 
             tempCanvas.height = height;
             tempCanvas.width = width;
@@ -118,6 +118,11 @@
             }
         //};
     }
+
+    $("#saveLink").click(function() {
+        var canvas = document.getElementById("newPicture");
+        Canvas2Image.saveAsPNG(canvas);
+    });
 
     /*processFriends(function() {
         //tile("512769141");

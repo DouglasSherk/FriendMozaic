3c333d8729af1fbec5805c19fe9528ef9808011e
Doug Sherk <github@sherk.me>
2012-01-21 07:24:32 +0000

    js stuff

--- js/tile.js
+++ js/tile.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
-    var uids = ["100003343000188", "510139148"];
-    var imageData = new array();
+    var tileX = 10;
+    var tileY = 10;
+
+    var uids = ["100003343000188", "510139148", "715822810", "504072909", "1649640112"];
+    var imgData = new Array();
+    var originalData = new Array();
     var context = document.getElementById("newPicture").getContext("2d");
 
     function processFriends(callback) {
@@ -9,31 +13,32 @@ $(document).ready(function() {
         var count = uids.length, num = 0;
         for (var uid in uids) {
             var image = new Image();
-            image.crossOrigin = 'anonymous';
-            image.src = "https://graph.facebook.com/" + uid + "/picture?type=square";
+            image.src = "proxySquare.php?uid=" + uids[uid];
+            image.uid = uids[uid];
             function imageLoaded(currentNum) {
                 if (currentNum == count) {
                     callback();
                 }
             }
             image.onload = function() {
-                tempContext.drawImage(image, 0, 0, 50, 50);
-                data = tempContext.getImageData(0, 0, 50, 50);
+                tempContext.drawImage(this, 0, 0, tileX, tileY);
+                var pixels = tempContext.getImageData(0, 0, tileX, tileY);
 
-                var avR, avG, avB;
-                for (var y = 0; y < 50; y++) {
-                    for (var x = 0; x < 50; x++) {
-                        var offset = (y * 50 + x) * 4; 
-                        avR += data[offset];
-                        avG += data[offset + 1];
-                        avB += data[offset + 2];
+                var avR = 0, avG = 0, avB = 0;
+                for (var y = 0; y < tileY; y++) {
+                    for (var x = 0; x < tileX; x++) {
+                        var offset = (y * tileY + x) * 4; 
+                        avR += pixels.data[offset];
+                        avG += pixels.data[offset + 1];
+                        avB += pixels.data[offset + 2];
                     }
                 }
 
                 avR = ~~(avR / 2500);
                 avG = ~~(avG / 2500);
                 avB = ~~(avB / 2500);
-                imageData[uid] = [avR, avG, avB];
+                imgData[this.uid] = [avR, avG, avB];
+                originalData[this.uid] = pixels;
 
                 imageLoaded(++num); 
             }
@@ -42,23 +47,24 @@ $(document).ready(function() {
     
     function tile(profilePic) {
         var image = new Image();
-        image.crossOrigin = 'anonymous';
-        image.src = "https://graph.facebook.com/" + profilePic + "/picture?type=large";
+        image.src = "proxyLarge.php?uid=" + profilePic;
+        // Where we're currently writing the thumb to.
+        var writeY = 0, writeX = 0;
         image.onload = function() {
             var tempCanvas = document.createElement("canvas");
 
             var height;
-            if (image.height % 50 >= 25) {
-                height = image.height + 50;
+            if (image.height % tileY >= tileY / 2) {
+                height = image.height + tileY;
             } else {
-                height = image.height - (image.height % 50);
+                height = image.height - (image.height % tileY);
             }
 
             var width;
-            if (image.width % 50 >= 25) {
-                width = image.width - (image.width % 50) + 50;
+            if (image.width % tileX >= tileX / 2) {
+                width = image.width - (image.width % tileX) + tileX;
             } else {
-                width = image.width - (image.width % 50);
+                width = image.width - (image.width % tileX);
             }
 
             tempCanvas.height = height;
@@ -66,30 +72,47 @@ $(document).ready(function() {
             var tempContext = tempCanvas.getContext("2d");
             tempContext.drawImage(image, 0, 0, width, height);
 
-            var imageData = tempContext.getImageData(0, 0, width, height);
-            for (var boxY = 0; boxY < height / 50; boxX++) {
-                for (var boxX = 0; boxX < width / 50; boxX++) { 
-                    var avR, avG, avB;
-                    for (var y = 0; y < 50; y++) {
-                        for (var x = 0; x < 50; x++) {
-                            var offset = ((boxY * 50 + y) * width + boxX * 50 + x) * 4;
-                            avR += offset[0];
-                            avG += offset[1];
-                            avB += offset[2];
+            var pixels = tempContext.getImageData(0, 0, width, height);
+            for (var boxY = 0; boxY < height / tileY; boxY++) {
+                for (var boxX = 0; boxX < width / tileX; boxX++) { 
+                    var avR = 0, avG = 0, avB = 0;
+                    for (var y = 0; y < tileY; y++) {
+                        for (var x = 0; x < tileX; x++) {
+                            var offset = ((boxY * tileY + y) * width + boxX * tileX + x) * 4;
+                            avR += pixels.data[offset];
+                            avG += pixels.data[offset + 1];
+                            avB += pixels.data[offset + 2];
                         }
                     }
                     avR = ~~(avR/2500);
                     avG = ~~(avG/2500);
                     avB = ~~(avB/2500);
-                }
 
-                for (var uid in uids) {
-                    
+                    var lowestDistance = 9999999999;
+                    var lowestDistanceUid = "";
+                    for (var uid in uids) {
+                        var friendData = imgData[uids[uid]];
+                        var distance = Math.abs(friendData[0] - avR) + Math.abs(friendData[1] - avG) + Math.abs(friendData[2] - avB);
+
+                        if (distance < lowestDistance) {
+                            lowestDistance = distance;
+                            lowestDistanceUid = uids[uid];
+                        }
+                    }
+
+                    context.putImageData(originalData[lowestDistanceUid], writeX * tileX, writeY * tileY);
+                    if (++writeX >= width / tileX) {
+                        writeX = 0;
+                        writeY++;
+                    }
                 }
             }
         };
     }
 
-    processFriends();
-    tile("512769141");
+    processFriends(function() {
+        //tile("512769141");
+        //tile("douglas.sherk");
+        tile("1658071309");
+    });
 });

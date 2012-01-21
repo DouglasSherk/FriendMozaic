aea0bb8ab1d8ca3c18e9f052cd0b12b3e0c7c112
Doug Sherk <github@sherk.me>
2012-01-21 00:02:40 +0000

    boilerplate js stuff

---
+++ js/tile.js
@@ -0,0 +1,9 @@
+var paths = ["res/img/stock1.jpg", "res/img/stock2.jpg"];
+
+function tile(profilePic, friendPictures) {
+    for (var picture in friendPictures) {
+        
+    }
+}
+
+tile("res/img/stock1.jpg", paths);
